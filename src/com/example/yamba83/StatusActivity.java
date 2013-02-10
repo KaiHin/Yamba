2e1db92896370b4f0f92979c64a7172748c6ad50
Kai Hin <kaihin6@gmail.com>
2013-02-10 20:29:57 +0100

    Version 1 of StatusActivity and update and grant internet persmission in AndroidManifest

--- src/com/example/yamba83/StatusActivity.java
+++ src/com/example/yamba83/StatusActivity.java
@@ -1,22 +1,39 @@
 package com.example.yamba83;
 
+import winterwell.jtwitter.Twitter;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
-
+public class StatusActivity extends Activity implements OnClickListener  { // Make StatusActivity capable of being a button listener, needs to implement onClickListener interface
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+		
+	/** Called when the activity is first created */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.status, menu);
-        return true;
-    }
+    	// Find views
+        editText = (EditText) findViewById(R.id.editText); // Find views inflated from xml layout and assign to Java variables
+        updateButton = (Button) findViewById(R.id.buttonUpdate);
+        
+        updateButton.setOnClickListener(this); // Registers button to notify 'this' (i.e. StausActivity) when it's clicked
+        
+        twitter = new Twitter("student", "password"); // Connect to online service that supports Twitter API, usrname and pwd hardcoded
+        twitter.setAPIRootUrl("http://yamba.marakana.com/api"); 
+        }
     
+    /** Called when button is clicked */
+    public void onClick(View v) {
+    	twitter.setStatus(editText.getText().toString()); // Make webservice API call to update the status
+    	Log.d(TAG,"onClicked");
+    }
 }
